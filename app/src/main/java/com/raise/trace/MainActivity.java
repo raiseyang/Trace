1842c03a2a14a1acf871aeaa8320ca5d3cf45d77
y22222ly@163.com <y22222ly@163.com>
2017-07-30 13:54:08 +0800

    1.0.1版，优化格式

--- app/src/main/java/com/raise/trace/MainActivity.java
+++ app/src/main/java/com/raise/trace/MainActivity.java
@@ -3,9 +3,14 @@ package com.raise.trace;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends Activity {
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,32 @@ public class MainActivity extends Activity {
     }
 
     public void click_test(View view) {
-        Toast.makeText(MainActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+        Trace.logPath(getCacheDir()+"/trace.log");
+        Trace.showCodePosition(true);
+
+        Map map = new HashMap();
+        map.put("name", "raise");
+        map.put("age", "18");
+
+        Set set = map.keySet();
+
+        Trace.d(" d");
+        Trace.i(" i");
+        Trace.w(" w");
+        Trace.e(" e = " + null);
+        Trace.d(TAG, " d");
+        Trace.i(TAG, " i");
+        Trace.w(TAG, " w");
+        Trace.e(TAG, " e = " + null);
+        Trace.d(TAG, " %s,%d", "raise", 1);
+        Trace.i(TAG, " %s,%d", "raise", 1);
+        Trace.w(TAG, " %s,%d", "raise", 1);
+        Trace.e(TAG, "333", new NullPointerException("fu*k null pointer exception."));
+        Trace.json(TAG, "{\"name\":\"BeJson\",\"url\":\"http://www.bejson.com\",\"page\":88,\"isNonProfit\":true}");
+        Trace.array(TAG, new String[]{"value1", "value2"});
+        Trace.list(TAG, Arrays.asList("list1", "list2", "list3"));
+        Trace.map(TAG, map);
+        Trace.set(TAG, set);
+        Trace.xml(TAG, "<student><age>12</age><name>jack</name><skill><language>chinese</language><run>22</run></skill></student>");
     }
 }
--- lib_trace/src/main/java/com/raise/trace/Trace.java
+++ lib_trace/src/main/java/com/raise/trace/Trace.java
@@ -16,11 +16,12 @@ import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.UnknownHostException;
-import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
@@ -57,38 +58,37 @@ public class Trace {
     private static int s_level;//日志级别
     private static boolean s_show_code_position; //是否显示 调用位置
     private static boolean s_write_file; //是否写文件
-    private static int offset;
+    private static int s_offset;
 
-    private static String m_log_path;
-    private static int s_log_size = 500;
+    private static String s_log_path;
+    private static int s_log_size = 500;// 日志文件大小，单位:kb
 
-    private static String global_tag;
+    private static String s_global_tag;// 全局tag
 
     static {
-        s_level = NONE;
+        s_level = DEBUG;
         s_show_code_position = false;
         s_write_file = true;
-        global_tag = "iport/";
-        m_log_path = Environment.getExternalStorageDirectory() + "/iport_log.txt";
-    }
-
-    public static void setShowPosition(boolean showPosition) {
-        s_show_code_position = showPosition;
+        s_global_tag = "trace";
+        s_log_path = Environment.getExternalStorageDirectory() + "/trace.log";
     }
 
     private static void println(int level, String tag, String message) {
         if (level >= s_level) {//过滤级别
+            if (tag.length() != 0) {
+                tag = "/" + tag;
+            }
             if (s_write_file) {
-                write_log(tag, message);
+                write_log_file(s_global_tag + tag, message);
             }
             if (s_show_code_position) {
-                message += getCodePosition();
+                message += get_code_position();
             }
-            printAndroidLog(level, global_tag + tag, message);
+            print_android_log(level, s_global_tag + tag, message);
         }
     }
 
-    private static void printAndroidLog(int level, String tag, String message) {
+    private static void print_android_log(int level, String tag, String message) {
         switch (level) {
             case VERBOSE:
                 Log.v(tag, message);
@@ -108,37 +108,68 @@ public class Trace {
         }
     }
 
-    public static void setLevel(int level) {
+    /**
+     * 显示代码定位
+     *
+     * @param is_show true if need show.
+     */
+    public static void showCodePosition(boolean is_show) {
+        s_show_code_position = is_show;
+    }
+
+    /**
+     * 设置日志显示级别<br/>
+     * 若不需要显示日志，请使用{@link #NONE}
+     *
+     * @param level
+     */
+    public static void level(int level) {
         s_level = level;
     }
 
     /**
      * 是否写日志文件
      *
-     * @param write_file
+     * @param is_write_file
      */
-    public static void write_file(boolean write_file) {
-        s_write_file = write_file;
+    public static void isWriteFile(boolean is_write_file) {
+        s_write_file = is_write_file;
     }
 
     /**
-     * 日志文件大小，单位：KB,超过此大小会重新读写文件,默认值 500
+     * 日志文件大小，单位：KB,超过此大小会产生一个备份文件,默认值 500
      */
-    public static void setLog_size(int log_size) {
+    public static void logSize(int log_size) {
         s_log_size = log_size;
     }
 
     /**
      * 设置日志保存路径
      *
-     * @param m_log_path
+     * @param log_path
      */
-    public static void setLog_path(String m_log_path) {
-        Trace.m_log_path = m_log_path;
+    public static void logPath(String log_path) {
+        Trace.s_log_path = log_path;
     }
 
     public static void v(String tag, String msg) {
         println(2, tag, msg);
+    }
+
+    public static void v(String msg) {
+        println(2, "", msg);
+    }
+
+    public static void v(String tag, String msg, Object... args) {
+        if (args.length == 1 && args[0] instanceof Throwable) {
+            println(2, tag, msg + '\n' + get_stack_trace_string((Throwable) args[0]));
+        } else {
+            println(2, tag, String.format(msg, args));
+        }
+    }
+
+    public static void d(String msg) {
+        println(3, "", msg);
     }
 
     public static void d(String tag, String msg) {
@@ -146,7 +177,15 @@ public class Trace {
     }
 
     public static void d(String tag, String msg, Object... args) {
-        println(3, tag, String.format(msg, args));
+        if (args.length == 1 && args[0] instanceof Throwable) {
+            println(3, tag, msg + '\n' + get_stack_trace_string((Throwable) args[0]));
+        } else {
+            println(3, tag, String.format(msg, args));
+        }
+    }
+
+    public static void i(String msg) {
+        println(4, "", msg);
     }
 
     public static void i(String tag, String msg) {
@@ -154,31 +193,47 @@ public class Trace {
     }
 
     public static void i(String tag, String msg, Object... args) {
-        println(4, tag, String.format(msg, args));
+        if (args.length == 1 && args[0] instanceof Throwable) {
+            println(4, tag, msg + '\n' + get_stack_trace_string((Throwable) args[0]));
+        } else {
+            println(4, tag, String.format(msg, args));
+        }
     }
 
-    public static void w(String tag, String msg, Object... args) {
-        println(5, tag, String.format(msg, args));
+    public static void w(String msg) {
+        println(5, "", msg);
     }
 
     public static void w(String tag, String msg) {
         println(5, tag, msg);
     }
 
+    public static void w(String tag, String msg, Object... args) {
+        if (args.length == 1 && args[0] instanceof Throwable) {
+            println(5, tag, msg + '\n' + get_stack_trace_string((Throwable) args[0]));
+        } else {
+            println(5, tag, String.format(msg, args));
+        }
+    }
+
+    public static void e(String msg) {
+        println(6, "", msg);
+    }
+
     public static void e(String tag, String msg) {
         println(6, tag, msg);
     }
 
-    public static void e(String tag, String msg, Throwable tr) {
-        println(6, tag, msg + '\n' + getStackTraceString(tr));
-    }
-
-    public static void e(String tag, Throwable tr) {
-        println(6, tag, getStackTraceString(tr));
+    public static void e(String tag, String msg, Object... args) {
+        if (args.length == 1 && args[0] instanceof Throwable) {
+            println(5, tag, msg + '\n' + get_stack_trace_string((Throwable) args[0]));
+        } else {
+            println(5, tag, String.format(msg, args));
+        }
     }
 
     public static <T> void array(String tag, T[] array) {
-        offset = 1;
+        s_offset = 1;
         if (array == null) {
             e(tag, "array is null");
         } else {
@@ -187,11 +242,10 @@ public class Trace {
     }
 
     public static void list(String tag, List<?> lists) {
-        offset = 1;
+        s_offset = 1;
         if (lists == null) {
             e(tag, "lists is null");
         } else {
-
             int iMax = lists.size() - 1;
             if (iMax == -1)
                 d(tag, "{}");
@@ -201,16 +255,61 @@ public class Trace {
                 for (int i = 0; i < lists.size(); i++) {
                     b.append(String.valueOf(lists.get(i)));
                     if (i == iMax)
-                        b.append('}');
-                    b.append(", ");
+                        b.append("}\n");
+                    else
+                        b.append(",\n");
                 }
                 d(tag, b.toString());
             }
         }
     }
 
+    public static void map(String tag, Map<?, ?> map) {
+        s_offset = 1;
+        if (map == null) {
+            e(tag, "map is null");
+        } else {
+            int iMax = map.size() - 1;
+            if (iMax == -1)
+                d(tag, "{}");
+            else {
+                StringBuilder b = new StringBuilder();
+                b.append('{');
+                for (Map.Entry<?, ?> entry : map.entrySet()) {
+                    b.append(";\nkey = ")
+                            .append(entry.getKey())
+                            .append(", value = ")
+                            .append(entry.getValue());
+                }
+                b.append("}\n");
+                d(tag, b.toString().replaceFirst(";\n", ""));
+            }
+        }
+    }
+
+    public static void set(String tag, Set<?> set) {
+        s_offset = 1;
+        if (set == null) {
+            e(tag, "set is null");
+        } else {
+            int iMax = set.size() - 1;
+            if (iMax == -1)
+                d(tag, "{}");
+            else {
+                StringBuilder b = new StringBuilder();
+                b.append('{');
+                for (Object entry : set) {
+                    b.append(",")
+                            .append(entry);
+                }
+                b.append('}');
+                d(tag, b.toString().replaceFirst(",", ""));
+            }
+        }
+    }
+
     public static void json(String tag, String json) {
-        offset = 1;
+        s_offset = 1;
         if (TextUtils.isEmpty(json)) {
             e(tag, "Empty/Null json content");
             return;
@@ -219,13 +318,13 @@ public class Trace {
             json = json.trim();
             if (json.startsWith("{")) {
                 JSONObject jsonObject = new JSONObject(json);
-                String message = jsonObject.toString(2);
+                String message = jsonObject.toString(2).replace("\\/", "/");
                 d(tag, message);
                 return;
             }
             if (json.startsWith("[")) {
                 JSONArray jsonArray = new JSONArray(json);
-                String message = jsonArray.toString(2);
+                String message = jsonArray.toString(2).replace("\\/", "/");
                 d(tag, message);
                 return;
             }
@@ -236,7 +335,7 @@ public class Trace {
     }
 
     public static void xml(String tag, String xml) {
-        offset = 1;
+        s_offset = 1;
         if (TextUtils.isEmpty(xml)) {
             e(tag, "Empty/Null xml content");
             return;
@@ -256,7 +355,7 @@ public class Trace {
     }
 
     public static void file(String tag, File file) {
-        offset = 1;
+        s_offset = 1;
         if (file == null || !file.exists()) {
             e(tag, "Empty/Null file");
             return;
@@ -265,7 +364,7 @@ public class Trace {
             FileInputStream fis = new FileInputStream(file);
             byte[] bytes = new byte[1024 * 4];
             fis.read(bytes);
-            String content = new String(bytes, Charset.defaultCharset());
+            String content = new String(bytes);
             String result = String.format("file name:%s,file size:%s\n%s", file.getName(), file.length(), content);
             d(tag, result);
         } catch (Exception e) {
@@ -273,18 +372,10 @@ public class Trace {
         }
     }
 
-    /**
-     * Handy function to get a loggable stack trace from a Throwable
-     *
-     * @param tr An exception to log
-     */
-    private static String getStackTraceString(Throwable tr) {
+    private static String get_stack_trace_string(Throwable tr) {
         if (tr == null) {
             return "";
         }
-
-        // This is to reduce the amount of log spew that apps do in the non-error
-        // condition of the network being unavailable.
         Throwable t = tr;
         while (t != null) {
             if (t instanceof UnknownHostException) {
@@ -292,7 +383,6 @@ public class Trace {
             }
             t = t.getCause();
         }
-
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         tr.printStackTrace(pw);
@@ -305,47 +395,45 @@ public class Trace {
      *
      * @return
      */
-    private static String getCodePosition() {
+    private static String get_code_position() {
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-
-        int index = 5 + offset;
-        offset = 0;
+        int index = 5 + s_offset;
+        s_offset = 0;
         String className = stackTrace[index].getFileName();
         String methodName = stackTrace[index].getMethodName();
         int lineNumber = stackTrace[index].getLineNumber();
-//       return " at "+stackTrace[index].toString();
         return String.format(".(%s:%s) %s()", className, lineNumber, methodName);
     }
 
     private static boolean check_log_file() {
-        File log_file = new File(m_log_path);
+        File log_file = new File(s_log_path);
         if (!log_file.exists()) {
             if (!log_file.getParentFile().exists()) {
                 boolean mkdirs = log_file.getParentFile().mkdirs();//父目录不存在创建
                 if (!mkdirs) {
-                    Log.e(global_tag, getStackTraceString(new IOException("Can't create the directory of trace. Please check the trace path.")));
+                    Log.e(s_global_tag, get_stack_trace_string(new IOException("Can't create the directory of trace. Please check the trace path.")));
                     return false;
                 }
             }
             try {
                 log_file.createNewFile();
             } catch (IOException e) {
-                Log.e("Trace", getStackTraceString(e));
+                Log.e("Trace", get_stack_trace_string(e));
                 return false;
             }
         } else {
             // 日志文件备份
             if (log_file.length() > 1024 * s_log_size)
-                log_file.renameTo(new File(m_log_path + "(1)"));
+                log_file.renameTo(new File(s_log_path + "(1)"));
         }
         return true;
     }
 
-    private static void write_log(String tag, String msg) {
-        File log_file = new File(m_log_path);
+    private static void write_log_file(String tag, String msg) {
+        File log_file = new File(s_log_path);
         if (!check_log_file()) return;
 
-        String text = getFormatLog(tag, msg);
+        String text = get_format_log(tag, msg);
         FileOutputStream fos = null;
         try {
             boolean append = log_file.length() <= 1024 * s_log_size;
@@ -364,17 +452,17 @@ public class Trace {
         }
     }
 
-    private static String getFormatLog(String tag, String msg) {
+    private static String get_format_log(String tag, String msg) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         String dateStr = sdf.format(new Date());
         return dateStr +
                 " " +
-                String.format("%s ", convertThreadId((int) Thread.currentThread().getId())) +
+                String.format("%s ", convert_thread_id((int) Thread.currentThread().getId())) +
                 String.format("%s: ", tag) +
                 msg;
     }
 
-    private static String convertThreadId(int value) {
+    private static String convert_thread_id(int value) {
         int limit = 5;
         String src = String.valueOf(value);
         int i = limit - src.length();
